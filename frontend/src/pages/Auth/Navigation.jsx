eb98495ddf4e994dbd05628b28435fde137d7d46
Garima9654 <myselfgarima118@gmail.com>
2024-04-22 19:40:25 +0530

    completed the user and admin profile

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -166,16 +166,13 @@ function Navigation() {
               </>
             )}
             <li>
-              <Link
-                to="/admin/profile"
-                className="block px-4 py-2 hover:bg-gray-100"
-              >
+              <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100">
                 Profile
               </Link>
             </li>
             <li>
               <Link
-                to="/admin/logout"
+                to="/logout"
                 onClick={logoutHandler}
                 className="block px-4 py-2 hover:bg-gray-100"
               >
